1839569d713ea6f828d899bc474bdaf6b59e97f8
caiovellani <caiovellani@outlook.com>
2024-07-15 22:16:18 -0300

    feat: create bottomNavBar components

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import '../components/bottom_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar:
+          MyBottomNavBar(onTabChange: (index) => navigateBottomBar(index)),
+    );
   }
 }
