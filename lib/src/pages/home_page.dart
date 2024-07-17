e2cfe421afc6e6612c4e1fe005587044b14c550b
caiovellani <caiovellani@outlook.com>
2024-07-16 21:30:08 -0300

    feat: finish home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../components/bottom_nav_bar.dart';
+import '../core/constants/image_constants.dart';
+import 'cart_page.dart';
+import 'shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,12 +21,118 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      bottomNavigationBar:
-          MyBottomNavBar(onTabChange: (index) => navigateBottomBar(index)),
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Theme(
+                  data: Theme.of(context).copyWith(
+                    dividerTheme:
+                        const DividerThemeData(color: Colors.transparent),
+                  ),
+                  child: DrawerHeader(
+                    child: Image.asset(
+                      ImageConstants.logo,
+                      color: Colors.white,
+                      width: 240,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Padding(
+                  padding: EdgeInsets.only(left: 24),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Colors.white,
+                      size: 32,
+                    ),
+                    title: Text(
+                      'About',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 24, bottom: 24),
+              child: ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                  size: 32,
+                ),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
