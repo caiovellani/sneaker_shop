c3df23e13e7be4ed52966454c116025db68f9123
caiovellani <caiovellani@outlook.com>
2024-07-16 22:58:58 -0300

    remove: "remove some files from project"

--- lib/core/constants/image_constants.dart
+++
@@ -1,8 +0,0 @@
-abstract class ImageConstants {
-  static const String adidas = 'assets/images/adidas.png';
-  static const String dunk = 'assets/images/dunk.png';
-  static const String jordan = 'assets/images/jordan.png';
-  static const String logo = 'assets/images/logo.png';
-  static const String nmd = 'assets/images/nmd.png';
-  static const String yeezy = 'assets/images/yeezy.png';
-}
