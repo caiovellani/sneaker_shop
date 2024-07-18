e4e03aa1f28c847e0e4630f9ded7df576f27cce5
caiovellani <caiovellani@outlook.com>
2024-07-17 21:53:41 -0300

    refactor: "update constants images"

--- lib/src/core/constants/image_constants.dart
+++ lib/src/core/constants/image_constants.dart
@@ -1,8 +1,8 @@
 abstract class ImageConstants {
-  static const String airforce = 'assets/images/airforce.png';
   static const String dunk = 'assets/images/dunk.png';
   static const String jordan = 'assets/images/jordan.png';
   static const String logo = 'assets/images/logo.png';
-  static const String blueoff = 'assets/images/blueoff.png';
-  static const String redoff = 'assets/images/redoff.png';
+  static const String red = 'assets/images/redoff.png';
+  static const String blue = 'assets/images/blueoff.png';
+  static const String airforce = 'assets/images/airforce.png';
 }
