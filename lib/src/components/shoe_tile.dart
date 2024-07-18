20c26b0f1264850184f0a25a1faffb19291b5633
caiovellani <caiovellani@outlook.com>
2024-07-17 21:53:27 -0300

    refactor: "add click to button, update padding"

--- lib/src/components/shoe_tile.dart
+++ lib/src/components/shoe_tile.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import '../../models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
+  final Function()? onTap;
   final Shoe shoe;
-  const ShoeTile({super.key, required this.shoe});
+  const ShoeTile({
+    super.key,
+    required this.shoe,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +30,13 @@ class ShoeTile extends StatelessWidget {
               child: Image.asset(shoe.imagePath),
             ),
           ),
-          Text(
-            shoe.description,
-            style: TextStyle(
-              color: Colors.grey[600],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Text(
+              shoe.description,
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
             ),
           ),
           Padding(
@@ -56,18 +64,21 @@ class ShoeTile extends StatelessWidget {
                     ),
                   ],
                 ),
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      bottomLeft: Radius.circular(12),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      ),
                     ),
-                  ),
-                  child: const Icon(
-                    Icons.add,
-                    color: Colors.white,
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
                   ),
                 )
               ],
